5cb4a8f60d410a30edabdf4a5f9a32e40e2f6fe8
Anthony I. Stewart <anthonystewart1129@gmail.com>
2020-12-11 12:21:17 -0500

    removed unused const x

--- frontend/src/components/HomeView.js
+++ frontend/src/components/HomeView.js
@@ -3,41 +3,37 @@ import axios from 'axios'
 import TemperatureWidget from './widgets/TemperatureWidget'
 import EnergyWidget from './widgets/EnergyWidget'
 
-const HomeView = ({ userData }) => {
-  const x = 1
-
-  return (
-    <>
-      <h3>
-        Welcome
-        {' '}
-        {(userData.first_name === '') ? userData.username : userData.first_name}
-        {'!'}
-      </h3>
-      <div className="row p-5">
-        {(userData.widget_1 === 1) && (
-          <div className="col-auto">
-            <EnergyWidget />
-          </div>
-        )}
-        {(userData.widget_1 === 2) && (
-          <div className="col-auto">
-            <TemperatureWidget />
-          </div>
-        )}
-        {(userData.widget_2 === 1) && (
-          <div className="col-auto">
-            <EnergyWidget />
-          </div>
-        )}
-        {(userData.widget_2 === 2) && (
-          <div className="col-auto">
-            <TemperatureWidget />
-          </div>
-        )}
-      </div>
-    </>
-  )
-}
+const HomeView = ({ userData }) => (
+  <>
+    <h3>
+      Welcome
+      {' '}
+      {(userData.first_name === '') ? userData.username : userData.first_name}
+      {'!'}
+    </h3>
+    <div className="row p-5">
+      {(userData.widget_1 === 1) && (
+        <div className="col-auto">
+          <EnergyWidget />
+        </div>
+      )}
+      {(userData.widget_1 === 2) && (
+        <div className="col-auto">
+          <TemperatureWidget />
+        </div>
+      )}
+      {(userData.widget_2 === 1) && (
+        <div className="col-auto">
+          <EnergyWidget />
+        </div>
+      )}
+      {(userData.widget_2 === 2) && (
+        <div className="col-auto">
+          <TemperatureWidget />
+        </div>
+      )}
+    </div>
+  </>
+)
 
 export default HomeView
